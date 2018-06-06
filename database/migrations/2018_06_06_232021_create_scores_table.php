c3b19ecafbd25c76e8938c160b83af9ead41575f
Ralfers <38739056+Ralfers@users.noreply.github.com>
2018-06-07 02:30:44 +0300

    Models

---
+++ database/migrations/2018_06_06_232021_create_scores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('user');
+            $table->integer('score');
+            $table->boolean('is_daily');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+}
